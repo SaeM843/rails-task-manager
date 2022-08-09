4b34384278c3db0295132fcd5a8d2ba81fd6cd9e
Sae Matsuda <sae.matsuda.843@gmail.com>
2022-08-09 15:52:45 +0900

    completed add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # form for adding a new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # send the information
+  post '/tasks', to: 'tasks#create'
+
+  # list all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # view the details of tasks
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
